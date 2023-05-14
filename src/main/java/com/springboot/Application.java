116ba5c978b07808824e6f2df16268998a829cb9
DESKTOP-BO4OKLN\user <jinwoo0302@naver.com>
2023-05-14 15:57:46 +0900

    Posts Domain 작성

---
+++ src/main/java/com/springboot/Application.java
@@ -0,0 +1,16 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    //단축키 psvm 개꿀
+    //단축키 alt + 1하면 프로젝트 창 닫고 열기
+    //lombok깔고 어디 들가서 체크하고 어플라이.
+    //gradle 바꾸면 무조건 reload
+
+}
