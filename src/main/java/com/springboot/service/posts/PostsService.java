9dcbba85dcf1d369720bd0d04c4686faf0ba44f8
DESKTOP-BO4OKLN\user <jinwoo0302@naver.com>
2023-05-21 20:05:48 +0900

    Mustache로 화면 구성하기

--- src/main/java/com/springboot/service/posts/PostsService.java
+++ src/main/java/com/springboot/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.springboot.service.posts;
 
 import com.springboot.Domain.posts.Posts;
 import com.springboot.Domain.posts.PostsRepository;
+import com.springboot.web.dto.PostsListResponseDto;
 import com.springboot.web.dto.PostsResponseDto;
 import com.springboot.web.dto.PostsSaveRequestDto;
 import com.springboot.web.dto.PostsUpdateRequestDto;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -23,6 +27,8 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+
+    @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
@@ -30,6 +36,24 @@ public class PostsService {
 
         return id;
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
